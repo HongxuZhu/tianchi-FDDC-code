ccf0b9711c5c5544cb8d2e1bd45204e258cd0e17
hongxuzhu <245651398@qq.com>
2018-05-29 19:00:49 +0800

    初始化项目

--- FDDC_PART2/nlp/tokenize/corenlp.py
+++ FDDC_PART2/nlp/tokenize/corenlp.py
@@ -1,12 +1,11 @@
 from stanfordcorenlp import StanfordCoreNLP
 
-# nlp = StanfordCoreNLP(r'G:\JavaLibraries\stanford-corenlp-full-2018-02-27')
+nlp = StanfordCoreNLP(r'/home/utopia/corpus/stanford-corenlp-full-2018-02-27', lang='zh')
 
 sentence = '清华大学位于北京。'
 
-with StanfordCoreNLP(r'/home/utopia/corpus/stanford-corenlp-full-2018-02-27', lang='zh') as nlp:
-    print(nlp.word_tokenize(sentence))
-    print(nlp.pos_tag(sentence))
-    print(nlp.ner(sentence))
-    print(nlp.parse(sentence))
-    print(nlp.dependency_parse(sentence))
+print(nlp.word_tokenize(sentence))
+# print(nlp.pos_tag(sentence))
+# print(nlp.ner(sentence))
+# print(nlp.parse(sentence))
+# print(nlp.dependency_parse(sentence))

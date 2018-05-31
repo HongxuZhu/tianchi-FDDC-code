f37fa6675454e8f10fe40229c9fd0e5568f5819d
hongxuzhu <245651398@qq.com>
2018-05-31 12:23:09 +0800

    初始化项目

--- FDDC_PART2/preprocessing/readTrainFile.py
+++ FDDC_PART2/preprocessing/readTrainFile.py
@@ -1,42 +1,33 @@
 import FDDC_PART2.preprocessing.htmlParser as paser
 import re
 
-trainpath = '/home/utopia/corpus/FDDC_part2_data/round1_train_20180518/定增/dingzeng.train'
-htmlpath = '/home/utopia/corpus/FDDC_part2_data/round1_train_20180518/定增/html/'
+# 公告id,增发对象,发行方式,增发数量,增发金额,锁定期,认购方式
+dz_trainpath = '/home/utopia/corpus/FDDC_part2_data/round1_train_20180518/定增/dingzeng.train'
+dz_htmlpath = '/home/utopia/corpus/FDDC_part2_data/round1_train_20180518/定增/html/'
+
+# 公告id,甲方,乙方,项目名称,合同名称,合同金额上限,合同金额下限,联合体成员
+ht_trainpath = '/home/utopia/corpus/FDDC_part2_data/round1_train_20180518/重大合同/hetong.train'
+ht_htmlpath = '/home/utopia/corpus/FDDC_part2_data/round1_train_20180518/重大合同/html/'
 
 
-# show 增发对象 header
-def show_obj_header():
-    with open(trainpath, 'r') as file:
-        for line in file:
-            entity = line.split('\t')
-            # 公告id,增发对象,发行方式,增发数量,增发金额,锁定期,认购方式
-            id = entity[0]
-            obj = entity[1]
-            # pub = entity[2]
-            # num = entity[3]
-            # amount = entity[4]
-            # lock = entity[5]
-            # sub = entity[6]
-            print(id, obj)
-
-
-def find_allheaders_fromhtml():
+def find_allheaders_fromhtml(trainpath, htmlpath, index):
     dict = {}
     with open(trainpath, 'r') as file:
         id = None
         for line in file:
             entity = line.split('\t')
-            if entity[0] != id:
-                id = entity[0]
-                obj = entity[1]
-                find_header_fromhtml(id, obj, dict)
+            length = len(entity)
+            if length > index:
+                if entity[0] != id:
+                    id = entity[0]
+                    val = entity[index]
+                    find_header_fromhtml(htmlpath, id, val, dict)
     print('----------------- over -----------------')
     print(sorted(dict.items(), key=lambda d: d[1], reverse=True))
 
 
 # 20503293 建信基金管理有限责任公司
-def find_header_fromhtml(id, val, dict):
+def find_header_fromhtml(htmlpath, id, val, dict):
     html = htmlpath + id + '.html'
     head = paser.show_header(html, val)
     print(id, val, head)
@@ -50,4 +41,4 @@ def find_header_fromhtml(id, val, dict):
         pass
 
 
-find_allheaders_fromhtml()
+find_allheaders_fromhtml(ht_trainpath, ht_htmlpath, 1)

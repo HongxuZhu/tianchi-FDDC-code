e3f19daa54d37710719c9888706b843d401f4f03
hongxuzhu <245651398@qq.com>
2018-06-12 18:20:18 +0800

    初始化项目

--- FDDC_PART2/expand/NER_IDCNN_CRF/predictor.py
+++ FDDC_PART2/expand/NER_IDCNN_CRF/predictor.py
@@ -1,7 +1,224 @@
-import FDDC_PART2.expand.NER_IDCNN_CRF.main as main
+# encoding=utf8
+import os
+import codecs
+import pickle
+import itertools
+from collections import OrderedDict
+import json
+import tensorflow as tf
+import numpy as np
+from model import Model
+from loader import load_sentences, update_tag_scheme
+from loader import char_mapping, tag_mapping
+from loader import augment_with_pretrained, prepare_dataset
+from utils import get_logger, make_path, clean, create_model, save_model
+from utils import print_config, save_config, load_config, test_ner
+from data_utils import load_word2vec, create_input, input_from_line, BatchManager
+from FDDC_PART2.preprocessing.htmlParser import levelText_withtable
 
-model, sess, char_to_id, id_to_tag = main.evaluate_line2()
-line = '中天科技是国内最早开发海底线缆产品的制造商，通过不断加大科研投入和自主技术创新，拥有多项海缆制造的核心技术，多年来，保持国内市场占有率第 一， 为国内第一个出口欧美的海底线缆制造商。为充分发挥公司海洋系列产品的优势，公司进一步延伸海底线缆业务板块，目前已形成海底光缆、海底电缆、接驳盒、水下连接器件等海洋系列产业链，并向海缆总包业务方向拓展，率先实现从海底线缆供应商向海缆项目整体解决方案系统集成商的转型。'
-result = model.evaluate_line(sess, main.input_from_line(line, char_to_id), id_to_tag)
+flags = tf.app.flags
+flags.DEFINE_boolean("clean", False, "clean train folder")
+flags.DEFINE_boolean("train", False, "Whether train the model")
+# configurations for the model
+flags.DEFINE_integer("seg_dim", 20, "Embedding size for segmentation, 0 if not used")
+flags.DEFINE_integer("char_dim", 100, "Embedding size for characters")
+flags.DEFINE_integer("lstm_dim", 100, "Num of hidden units in LSTM, or num of filters in IDCNN")
+flags.DEFINE_string("tag_schema", "iobes", "tagging schema iobes or iob")
 
-print(result)
+# configurations for training
+flags.DEFINE_float("clip", 5, "Gradient clip")
+flags.DEFINE_float("dropout", 0.5, "Dropout rate")
+flags.DEFINE_integer("batch_size", 100, "batch size")
+flags.DEFINE_float("lr", 0.001, "Initial learning rate")
+flags.DEFINE_string("optimizer", "adam", "Optimizer for training")
+flags.DEFINE_boolean("pre_emb", True, "Wither use pre-trained embedding")
+flags.DEFINE_boolean("zeros", False, "Wither replace digits with zero")
+flags.DEFINE_boolean("lower", True, "Wither lower case")
+
+flags.DEFINE_integer("max_epoch", 100, "maximum training epochs")
+flags.DEFINE_integer("steps_check", 100, "steps per checkpoint")
+flags.DEFINE_string("ckpt_path", "ckpt", "Path to save model")
+flags.DEFINE_string("summary_path", "summary", "Path to store summaries")
+flags.DEFINE_string("log_file", "train.log", "File for log")
+flags.DEFINE_string("map_file", "maps.pkl", "file for maps")
+flags.DEFINE_string("vocab_file", "vocab.json", "File for vocab")
+flags.DEFINE_string("config_file", "config_file", "File for config")
+flags.DEFINE_string("script", "conlleval", "evaluation script")
+flags.DEFINE_string("result_path", "result", "Path for results")
+flags.DEFINE_string("emb_file", os.path.join("data", "vec.txt"), "Path for pre_trained embedding")
+flags.DEFINE_string("train_file", os.path.join("data", "ht.train"), "Path for train data")
+flags.DEFINE_string("dev_file", os.path.join("data", "ht.dev"), "Path for dev data")
+flags.DEFINE_string("test_file", os.path.join("data", "ht.test"), "Path for test data")
+
+flags.DEFINE_string("model_type", "idcnn", "Model type, can be idcnn or bilstm")
+# flags.DEFINE_string("model_type", "bilstm", "Model type, can be idcnn or bilstm")
+
+FLAGS = tf.app.flags.FLAGS
+assert FLAGS.clip < 5.1, "gradient clip should't be too much"
+assert 0 <= FLAGS.dropout < 1, "dropout rate between 0 and 1"
+assert FLAGS.lr > 0, "learning rate must larger than zero"
+assert FLAGS.optimizer in ["adam", "sgd", "adagrad"]
+
+import os
+
+os.chdir('/home/utopia/PycharmProjects/csahsaohdoashdoasdhoa/FDDC_PART2/expand/NER_IDCNN_CRF')
+
+
+# config for the model
+def config_model(char_to_id, tag_to_id):
+    config = OrderedDict()
+    config["model_type"] = FLAGS.model_type
+    config["num_chars"] = len(char_to_id)
+    config["char_dim"] = FLAGS.char_dim
+    config["num_tags"] = len(tag_to_id)
+    config["seg_dim"] = FLAGS.seg_dim
+    config["lstm_dim"] = FLAGS.lstm_dim
+    config["batch_size"] = FLAGS.batch_size
+
+    config["emb_file"] = FLAGS.emb_file
+    config["clip"] = FLAGS.clip
+    config["dropout_keep"] = 1.0 - FLAGS.dropout
+    config["optimizer"] = FLAGS.optimizer
+    config["lr"] = FLAGS.lr
+    config["tag_schema"] = FLAGS.tag_schema
+    config["pre_emb"] = FLAGS.pre_emb
+    config["zeros"] = FLAGS.zeros
+    config["lower"] = FLAGS.lower
+    return config
+
+
+def evaluate(sess, model, name, data, id_to_tag, logger):
+    logger.info("evaluate:{}".format(name))
+    ner_results = model.evaluate(sess, data, id_to_tag)
+    eval_lines = test_ner(ner_results, FLAGS.result_path)
+    for line in eval_lines:
+        logger.info(line)
+    f1 = float(eval_lines[1].strip().split()[-1])
+
+    if name == "dev":
+        best_test_f1 = model.best_dev_f1.eval()
+        if f1 > best_test_f1:
+            tf.assign(model.best_dev_f1, f1).eval()
+            logger.info("new best dev f1 score:{:>.3f}".format(f1))
+        return f1 > best_test_f1
+    elif name == "test":
+        best_test_f1 = model.best_test_f1.eval()
+        if f1 > best_test_f1:
+            tf.assign(model.best_test_f1, f1).eval()
+            logger.info("new best test f1 score:{:>.3f}".format(f1))
+        return f1 > best_test_f1
+
+
+def train():
+    # load data sets
+    train_sentences = load_sentences(FLAGS.train_file, FLAGS.lower, FLAGS.zeros)
+    dev_sentences = load_sentences(FLAGS.dev_file, FLAGS.lower, FLAGS.zeros)
+    test_sentences = load_sentences(FLAGS.test_file, FLAGS.lower, FLAGS.zeros)
+
+    # Use selected tagging scheme (IOB / IOBES)
+    update_tag_scheme(train_sentences, FLAGS.tag_schema)
+    update_tag_scheme(test_sentences, FLAGS.tag_schema)
+
+    # create maps if not exist
+    if not os.path.isfile(FLAGS.map_file):
+        # create dictionary for word
+        if FLAGS.pre_emb:
+            dico_chars_train = char_mapping(train_sentences, FLAGS.lower)[0]
+            dico_chars, char_to_id, id_to_char = augment_with_pretrained(
+                dico_chars_train.copy(),
+                FLAGS.emb_file,
+                list(itertools.chain.from_iterable(
+                    [[w[0] for w in s] for s in test_sentences])
+                )
+            )
+        else:
+            _c, char_to_id, id_to_char = char_mapping(train_sentences, FLAGS.lower)
+
+        # Create a dictionary and a mapping for tags
+        _t, tag_to_id, id_to_tag = tag_mapping(train_sentences)
+        with open(FLAGS.map_file, "wb") as f:
+            pickle.dump([char_to_id, id_to_char, tag_to_id, id_to_tag], f)
+    else:
+        with open(FLAGS.map_file, "rb") as f:
+            char_to_id, id_to_char, tag_to_id, id_to_tag = pickle.load(f)
+
+    # prepare data, get a collection of list containing index
+    train_data = prepare_dataset(
+        train_sentences, char_to_id, tag_to_id, FLAGS.lower
+    )
+    dev_data = prepare_dataset(
+        dev_sentences, char_to_id, tag_to_id, FLAGS.lower
+    )
+    test_data = prepare_dataset(
+        test_sentences, char_to_id, tag_to_id, FLAGS.lower
+    )
+    print("%i / %i / %i sentences in train / dev / test." % (
+        len(train_data), len(dev_data), len(test_data)))
+
+    train_manager = BatchManager(train_data, FLAGS.batch_size)
+    dev_manager = BatchManager(dev_data, 100)
+    test_manager = BatchManager(test_data, 100)
+    # make path for store log and model if not exist
+    make_path(FLAGS)
+    if os.path.isfile(FLAGS.config_file):
+        config = load_config(FLAGS.config_file)
+    else:
+        config = config_model(char_to_id, tag_to_id)
+        save_config(config, FLAGS.config_file)
+    make_path(FLAGS)
+
+    log_path = os.path.join("log", FLAGS.log_file)
+    logger = get_logger(log_path)
+    print_config(config, logger)
+
+    # limit GPU memory
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    steps_per_epoch = train_manager.len_data
+    with tf.Session(config=tf_config) as sess:
+        model = create_model(sess, Model, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
+        logger.info("start training")
+        loss = []
+        for i in range(100):
+            for batch in train_manager.iter_batch(shuffle=True):
+                step, batch_loss = model.run_step(sess, True, batch)
+                loss.append(batch_loss)
+                if step % FLAGS.steps_check == 0:
+                    iteration = step // steps_per_epoch + 1
+                    logger.info("iteration:{} step:{}/{}, "
+                                "NER loss:{:>9.6f}".format(
+                        iteration, step % steps_per_epoch, steps_per_epoch, np.mean(loss)))
+                    loss = []
+
+            best = evaluate(sess, model, "dev", dev_manager, id_to_tag, logger)
+            if best:
+                save_model(sess, model, FLAGS.ckpt_path, logger)
+            # evaluate(sess, model, "test", test_manager, id_to_tag, logger)
+
+
+def evaluate_line_ht(htmlpath):
+    config = load_config(FLAGS.config_file)
+    logger = get_logger(FLAGS.log_file)
+    # limit GPU memory
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    with open(FLAGS.map_file, "rb") as f:
+        char_to_id, id_to_char, tag_to_id, id_to_tag = pickle.load(f)
+    with tf.Session(config=tf_config) as sess:
+        s_arr = levelText_withtable(htmlpath)
+        model = create_model(sess, Model, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
+        for sen in s_arr:
+            result = model.evaluate_line(sess, input_from_line(sen, char_to_id), id_to_tag)
+            entities = result.get('entities')
+            if len(entities) > 0:
+                for en in entities:
+                    print(en)
+
+
+def main(_):
+    line = '近日，江苏中天科技股份有限公司（上证代码： 600522 ，以下简称“中天科技股份”或“公司”）及控股子公司中天科技海缆有限公司（以下简称“中天科技海缆”）分别收到中标通知书，确认中天科技股份为盛东如东海上风力发电有限责任公司“海装如东海上风电场工程（如东 H3# ）海底光电复合电缆及附件”招标项目的中标人、中天科技海缆为“国家电网公司输变电项目 2017 年（新增）变电设备（含电缆）招标采购-电力电缆及电缆附件”招标项目的中标人，现将中标情况公告如下：'
+    evaluate_line_ht('/home/utopia/corpus/FDDC_part2_data/round1_train_20180518/重大合同/html/16773644.html')
+
+
+if __name__ == "__main__":
+    tf.app.run(main)

5330b47d18de9f7b85cbf36c0c71a13655bc0ba7
hongxuzhu <245651398@qq.com>
2018-06-15 14:00:01 +0800

    初始化项目

--- FDDC_PART2/preprocessing/autoTagging.py
+++ FDDC_PART2/preprocessing/autoTagging.py
@@ -26,6 +26,39 @@ def tag_text(htmlpath, trainline, makefile):
         pass
 
 
+def tagContract(htmlpath, conarray, makefile):
+    s_arr = parser.levelText_withtable(htmlpath)
+    for s in s_arr:
+        mask_contractUnion(s, conarray, makefile)
+    for contract in conarray:
+        if contract.count != contract.countActual:
+            contract.desc()
+
+
+def getContract(trainline):
+    contract = Contract(trainline)
+    return contract
+
+
+def mask_contractUnion(line_source, conarray, makefile):
+    tag_arr = ['O'] * len(line_source)
+    for contract in conarray:
+        mask_contract_field(line_source, contract.jiafang, tag_arr, 'JF', contract)
+        mask_contract_field(line_source, contract.yifang, tag_arr, 'YF', contract)
+        mask_contract_field(line_source, contract.xiangmu, tag_arr, 'XM', contract)
+        mask_contract_field(line_source, contract.hetong, tag_arr, 'HT', contract)
+        mask_contract_field(line_source, contract.amount_u, tag_arr, 'AU', contract)
+        mask_contract_field(line_source, contract.amount_d, tag_arr, 'AD', contract)
+
+        lhs = contract.lianhe.split('、')
+        for lh in lhs:
+            mask_contract_field(line_source, lh, tag_arr, 'LH', contract)
+
+    for i in range(len(line_source)):
+        makefile.write(line_source[i] + ' ' + tag_arr[i] + '\n')
+    makefile.write('\n')
+
+
 # 假设各字段之间不相交，对于金额上下限等情况考虑加入附加字段
 def mask_contract(line_source, contract, makefile):
     tag_arr = ['O'] * len(line_source)

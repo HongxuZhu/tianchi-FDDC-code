c26480699fcfe3160e946a3fbe8d25efef9baae8
hongxuzhu <245651398@qq.com>
2018-07-10 11:34:04 +0800

    初始化项目

--- FDDC_PART2_V1/preprocess/hetong/Trainfile_HT_ALL_NER_TEXT.py
+++ FDDC_PART2_V1/preprocess/hetong/Trainfile_HT_ALL_NER_TEXT.py
@@ -46,21 +46,21 @@ def showText(before=0):
             tag_arr = ['O'] * len(sentence)
             isMask = False
             for ht in hts:
-                if mask_contract_field(sentence, ht.jiafang, tag_arr, 'JF', ht, False):
+                if mask_contract_field_char(sentence, ht.jiafang, tag_arr, 'JF', ht, False):
                     isMask = True
-                if mask_contract_field(sentence, ht.yifang, tag_arr, 'YF', ht, False):
+                if mask_contract_field_char(sentence, ht.yifang, tag_arr, 'YF', ht, False):
                     isMask = True
-                if mask_contract_field(sentence, ht.xiangmu, tag_arr, 'XM', ht, False):
+                if mask_contract_field_char(sentence, ht.xiangmu, tag_arr, 'XM', ht, False):
                     isMask = True
-                if mask_contract_field(sentence, ht.hetong, tag_arr, 'HT', ht, False):
+                if mask_contract_field_char(sentence, ht.hetong, tag_arr, 'HT', ht, False):
                     isMask = True
-                if mask_contract_field(sentence, ht.amount_u, tag_arr, 'AU', ht, True):
+                if mask_contract_field_char(sentence, ht.amount_u, tag_arr, 'AU', ht, True):
                     isMask = True
-                if mask_contract_field(sentence, ht.amount_d, tag_arr, 'AD', ht, True):
+                if mask_contract_field_char(sentence, ht.amount_d, tag_arr, 'AD', ht, True):
                     isMask = True
                 lhs = ht.lianhe.split('、')
                 for lh in lhs:
-                    if mask_contract_field(sentence, lh, tag_arr, 'LH', ht, False):
+                    if mask_contract_field_char(sentence, lh, tag_arr, 'LH', ht, False):
                         isMask = True
             if isMask:
                 pass
@@ -84,35 +84,40 @@ def discoverText(before=0):
                 if i >= 0:
                     beforetext += sentences[i]
             context = beforetext + sentence
-            ywset = ltp_pos_v_distinct(context)
+
             tag_arr = ['O'] * len(sentence)
             isMask = False
             for ht in hts:
                 '''
                 setCount(dx_dict, ywset, id) 打开哪个注释，则看哪个字段的关键词
                 '''
-                if mask_contract_field(sentence, ht.jiafang, tag_arr, 'JF', ht, False):
+                if mask_contract_field_char(sentence, ht.jiafang, tag_arr, 'JF', ht, False):
+                    prefix = sentence[0:sentence.find(ht.jiafang)]
+                    ywset = ltp_pos_v_distinct(prefix)
+                    setCount(dx_dict, ywset, id)
+                    isMask = True
+                if mask_contract_field_char(sentence, ht.yifang, tag_arr, 'YF', ht, False):
                     # setCount(dx_dict, ywset, id)
                     isMask = True
-                if mask_contract_field(sentence, ht.yifang, tag_arr, 'YF', ht, False):
+                if mask_contract_field_char(sentence, ht.xiangmu, tag_arr, 'XM', ht, False):
                     # setCount(dx_dict, ywset, id)
                     isMask = True
-                if mask_contract_field(sentence, ht.xiangmu, tag_arr, 'XM', ht, False):
+                if mask_contract_field_char(sentence, ht.hetong, tag_arr, 'HT', ht, False):
                     # setCount(dx_dict, ywset, id)
                     isMask = True
-                if mask_contract_field(sentence, ht.hetong, tag_arr, 'HT', ht, False):
+                if mask_contract_field_char(sentence, ht.amount_u, tag_arr, 'AU', ht, True):
                     # setCount(dx_dict, ywset, id)
                     isMask = True
-                if mask_contract_field(sentence, ht.amount_u, tag_arr, 'AU', ht, True):
-                    # setCount(dx_dict, ywset, id)
-                    isMask = True
-                if mask_contract_field(sentence, ht.amount_d, tag_arr, 'AD', ht, True):
+                if mask_contract_field_char(sentence, ht.amount_d, tag_arr, 'AD', ht, True):
                     # setCount(dx_dict, ywset, id)
                     isMask = True
                 lhs = ht.lianhe.split('、')
                 for lh in lhs:
-                    if mask_contract_field(sentence, lh, tag_arr, 'LH', ht, False):
-                        setCount(dx_dict, ywset, id)
+                    if mask_contract_field_char(sentence, lh, tag_arr, 'LH', ht, False):
+                        # prefix = sentence[0:sentence.find(lh)]
+                        # ywset = ltp_pos_v_distinct(prefix)
+                        # setCount(dx_dict, ywset, id)
+                        isMask = True
 
     guolv = {k: len(v) for k, v in dx_dict.items() if len(v) > 9}
     paixu = sorted(guolv.items(), key=lambda d: d[1], reverse=True)
@@ -121,7 +126,7 @@ def discoverText(before=0):
         print(px[0] + '\t' + str(px[1]))
 
 
-def maketrain(before=0):
+def maketrain_char(before=0):
     rank = 6
     ht_train = open(dataroot + 'ht_all_text.train', 'a+')
     ht_dev = open(dataroot + 'ht_all_text.dev', 'a+')
@@ -150,29 +155,29 @@ def maketrain(before=0):
             tag_arr = ['O'] * len(context)
             isMask = False
             for ht in hts:
-                if mask_contract_field(context, ht.jiafang, tag_arr, 'JF', ht, False):
+                if mask_contract_field_char(context, ht.jiafang, tag_arr, 'JF', ht, False):
                     labels.add('__label__jf')
                     isMask = True
-                if mask_contract_field(context, ht.yifang, tag_arr, 'YF', ht, False):
+                if mask_contract_field_char(context, ht.yifang, tag_arr, 'YF', ht, False):
                     labels.add('__label__yf')
                     isMask = True
-                if mask_contract_field(context, ht.xiangmu, tag_arr, 'XM', ht, False):
+                if mask_contract_field_char(context, ht.xiangmu, tag_arr, 'XM', ht, False):
                     labels.add('__label__xm')
                     isMask = True
-                if mask_contract_field(context, ht.hetong, tag_arr, 'HT', ht, False):
+                if mask_contract_field_char(context, ht.hetong, tag_arr, 'HT', ht, False):
                     labels.add('__label__ht')
                     isMask = True
-                if mask_contract_field(context, ht.amount_u, tag_arr, 'AU', ht, True):
+                if mask_contract_field_char(context, ht.amount_u, tag_arr, 'AU', ht, True):
                     labels.add('__label__amount')
                     isMask = True
-                if mask_contract_field(context, ht.amount_d, tag_arr, 'AD', ht, True):
+                if mask_contract_field_char(context, ht.amount_d, tag_arr, 'AD', ht, True):
                     labels.add('__label__amount')
                     isMask = True
                 lhs = ht.lianhe.split('、')
 
                 # if filter_lh(context):
                 for lh in lhs:
-                    if mask_contract_field(context, lh, tag_arr, 'LH', ht, False):
+                    if mask_contract_field_char(context, lh, tag_arr, 'LH', ht, False):
                         labels.add('__label__lht')
                         isMask = True
             if True:
@@ -187,6 +192,122 @@ def maketrain(before=0):
 
         for ht in hts:
             ht.desc()
+
+
+def maketrain_word(before=0):
+    rank = 7
+    ht_train = open(dataroot + 'ht_all_text_word.train', 'a+')
+    ht_dev = open(dataroot + 'ht_all_text_word.dev', 'a+')
+    ht_test = open(dataroot + 'ht_all_text_word.test', 'a+')
+    cls_train = open('ht_cls_text_word.train', 'a+')
+    cls_dev = open('ht_cls_text_word.dev', 'a+')
+    hetongs = getHeTongUnion(dz_trainpath)
+    for id in hetongs.keys():
+        mod = int(id) % rank
+        if mod == 0:
+            nerfile = ht_dev
+            clsfile = cls_dev
+        elif mod == 1:
+            nerfile = ht_test
+            clsfile = cls_dev
+        else:
+            nerfile = ht_train
+            clsfile = cls_train
+        hts = hetongs[id]
+        htmlpath = dz_htmlpath + id + '.html'
+        sentences = levelText_without_table(htmlpath)
+        for sid in range(len(sentences)):
+            sentence = sentences[sid]
+            beforetext = ''
+            for i in range(sid - before, sid):
+                if i >= 0:
+                    beforetext += sentences[i]
+            context = beforetext + sentence
+            cws_cut_cent = jieba_tokenize(context)
+            labels = set()
+            tag_arr = ['O'] * len(cws_cut_cent)
+            isMask = False
+            for ht in hts:
+                if mask_contract_field_word(context, cws_cut_cent, ht.jiafang, tag_arr, 'JF', ht, False):
+                    labels.add('__label__jf')
+                    isMask = True
+                if mask_contract_field_word(context, cws_cut_cent, ht.yifang, tag_arr, 'YF', ht, False):
+                    labels.add('__label__yf')
+                    isMask = True
+                if mask_contract_field_word(context, cws_cut_cent, ht.xiangmu, tag_arr, 'XM', ht, False):
+                    labels.add('__label__xm')
+                    isMask = True
+                if mask_contract_field_word(context, cws_cut_cent, ht.hetong, tag_arr, 'HT', ht, False):
+                    labels.add('__label__ht')
+                    isMask = True
+                if mask_contract_field_word(context, cws_cut_cent, ht.amount_u, tag_arr, 'AU', ht, True):
+                    labels.add('__label__amount')
+                    isMask = True
+                if mask_contract_field_word(context, cws_cut_cent, ht.amount_d, tag_arr, 'AD', ht, True):
+                    labels.add('__label__amount')
+                    isMask = True
+                lhs = ht.lianhe.split('、')
+
+                # if filter_lh(context):
+                for lh in lhs:
+                    if mask_contract_field_word(context, cws_cut_cent, lh, tag_arr, 'LH', ht, False):
+                        labels.add('__label__lht')
+                        isMask = True
+            if True:
+                # for i in range(len(context)):
+                #     # nerfile.write(str(i + 1) + '\t' + context[i] + '\t' + tag_arr[i] + '\n')
+                #     nerfile.write(context[i] + ' ' + tag_arr[i] + '\n')
+                # nerfile.write('\n')
+
+                for i in range(len(cws_cut_cent)):
+                    nerfile.write(str(i + 1) + '\t' + cws_cut_cent[i] + '\t' + tag_arr[i] + '\n')
+                nerfile.write('\n')
+            else:
+                labels.add('__label__nothing')
+
+            # toline = ' '.join(labels) + ' ' + ' '.join(jieba_tokenize(context)) + '\n'
+            # clsfile.write(toline)
+
+        for ht in hts:
+            ht.desc()
+
+
+def maketrain_word_debug(id, before=0):
+    hetongs = getHeTongUnion(dz_trainpath)
+    hts = hetongs[id]
+    htmlpath = dz_htmlpath + id + '.html'
+    sentences = levelText_without_table(htmlpath)
+    for sid in range(len(sentences)):
+        sentence = sentences[sid]
+        beforetext = ''
+        for i in range(sid - before, sid):
+            if i >= 0:
+                beforetext += sentences[i]
+        context = beforetext + sentence
+        cws_cut_cent = jieba_tokenize(context)
+        labels = set()
+        tag_arr = ['O'] * len(cws_cut_cent)
+        for ht in hts:
+            if mask_contract_field_word(context, cws_cut_cent, ht.jiafang, tag_arr, 'JF', ht, False):
+                labels.add('__label__jf')
+            if mask_contract_field_word(context, cws_cut_cent, ht.yifang, tag_arr, 'YF', ht, False):
+                labels.add('__label__yf')
+            if mask_contract_field_word(context, cws_cut_cent, ht.xiangmu, tag_arr, 'XM', ht, False):
+                labels.add('__label__xm')
+            if mask_contract_field_word(context, cws_cut_cent, ht.hetong, tag_arr, 'HT', ht, False):
+                labels.add('__label__ht')
+            if mask_contract_field_word(context, cws_cut_cent, ht.amount_u, tag_arr, 'AU', ht, True):
+                labels.add('__label__amount')
+            if mask_contract_field_word(context, cws_cut_cent, ht.amount_d, tag_arr, 'AD', ht, True):
+                labels.add('__label__amount')
+            lhs = ht.lianhe.split('、')
+
+            for lh in lhs:
+                if mask_contract_field_word(context, cws_cut_cent, lh, tag_arr, 'LH', ht, False):
+                    labels.add('__label__lht')
+
+    for ht in hts:
+        ht.desc()
 
 
 def setCount(ywdict, ywset, id):
@@ -252,7 +373,7 @@ def levelText_without_table(htmlpath):
 #     return sents_arr
 
 
-def mask_contract_field(line_source, val, tag_arr, label, obj, isnum):
+def mask_contract_field_char(line_source, val, tag_arr, label, obj, isnum):
     isMask = False
     if val != 'fddcUndefined':
         val = val.replace('(', '\(')
@@ -290,6 +411,44 @@ def mask_contract_field(line_source, val, tag_arr, label, obj, isnum):
     return isMask
 
 
-# maketrain(before=0)
+def finditer(cws_cut_cent, val):
+    iters = []
+    begin = end = -1
+    for i in range(len(cws_cut_cent)):
+        cws = cws_cut_cent[i]
+        if val.startswith(cws):
+            begin = i
+        if val.endswith(cws):
+            end = i + 1
+        if begin != -1 and end > begin:
+            tmp = ''.join(cws_cut_cent[begin:end])
+            if tmp == val:
+                iters.append((begin, end))
+                begin = end = -1
+
+    return iters
+
+
+def mask_contract_field_word(context, cws_cut_cent, val, tag_arr, label, obj, isnum):
+    isMask = False
+    if val != 'fddcUndefined':
+        if context.find(val) != -1:
+            iters = finditer(cws_cut_cent, val)
+            for begin, end in iters:
+                obj.setStatus(label)
+                ner_label = label
+                tag_arr[begin] = 'B-' + ner_label
+                for index in range(begin + 1, end):
+                    tag_arr[index] = 'I-' + ner_label
+                isMask = True
+
+    return isMask
+
+
+maketrain_word()
+
+# maketrain_word_debug('1380579')
+
+# maketrain_char(before=0)
 # showText()
-# discoverText(before=1)
+# discoverText(before=0)
